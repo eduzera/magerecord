bc01a2993be57b6e0df01704c343f0b6ee60975e
Eduardo Zaghi <eduardozaghi@gmail.com>
2014-08-27 20:25:37 -0300

    remove coments

--- lib/magerecord/magento_database.rb
+++ lib/magerecord/magento_database.rb
@@ -1,7 +1,7 @@
 module MageRecord
   class MagentoDatabase < ActiveRecord::Base
     self.abstract_class = true
-    
+
     def self.connect(options={})
       MageRecord::MagentoDatabase.establish_connection(
         adapter: options[:adapter],
@@ -14,11 +14,3 @@ module MageRecord
     end
   end
 end
-
-# samples for test on irb
-
-# $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'lib'))
-# require 'magerecord'
-
-# MageRecord::MagentoDatabase.connect(adapter: 'mysql2', host: 'mysql02-farm19.uni5.net', database: 'pontafirme01', username: 'pontafirme01', password: 'moleque', secure_auth: false)
-# orders = MageRecord::Order.all.order('created_at ASC').limit(10)
