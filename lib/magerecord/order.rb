cd6613f6fdc835407e5a2caa6a1fac217a1cd9d5
Eduardo Zaghi <eduardozaghi@gmail.com>
2015-03-06 12:28:25 -0300

    add scope only complete for order

--- lib/magerecord/order.rb
+++ lib/magerecord/order.rb
@@ -6,6 +6,8 @@ module MageRecord
     # ignore canceled orders
     default_scope { where status: [:processing, :complete] }
 
+    scope :only_complete, -> {where status: [:complete]}
+
     belongs_to :customer
 
     has_one :billing_address, -> { where address_type: 'billing' }, class_name: :OrderAddress, foreign_key: :parent_id
